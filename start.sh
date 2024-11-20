acf3110ae00e255b76761be52d8984b2960fdbb6
Frablock <53878484+Frablock@users.noreply.github.com>
2024-11-20 18:37:56 +0100

    a

--- start.sh
+++ start.sh
@@ -8,7 +8,6 @@ if [ ! -d "$VENV_PATH" ]; then
     echo "Virtual environment not found at $VENV_PATH"
     echo "Creating venv"
     python -m venv $VENV_PATH
-    exit 1
 fi
 
 # Activate the virtual environment
