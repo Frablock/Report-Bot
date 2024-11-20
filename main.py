3e5fc4099904d80decc16df52934420e1d37190d
Frablock <53878484+Frablock@users.noreply.github.com>
2024-11-20 18:30:56 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+from dotenv import dotenv_values
+
+from interactions import OptionType, slash_option, slash_command, SlashContext, Embed, SlashCommandChoice
+import interactions
+
+from interactions import Client, Intents, listen
+
+
+import asyncio
+
+import random
+
+import sqlite3
+
+bot = Client(intents=Intents.DEFAULT)
+TOKEN = dotenv_values(".env")["TOKEN"]
+bot = interactions.Client(token=TOKEN)
+
+con = sqlite3.connect("reports.db")
+
+cur = con.cursor()
+
+@slash_command(name="report", description="Report a user/link/profile on a social-network")
+@slash_option(
+    name="link",
+    description="Link to the user profile",
+    required=True,
+    opt_type=OptionType.STRING
+)
+@slash_option(
+    name="Why ?",
+    description="Why did you report this profile ?",
+    required=True,
+    opt_type=OptionType.STRING
+)
+async def report(ctx: SlashContext, link, why):
+    embed = Embed(
+        title="Report",
+        description=ctx.locale+" "+link+" "+,
+        color=0xff0000 # RED color
+    )
+    await ctx.send(embed=embed)
+
+bot.start()
