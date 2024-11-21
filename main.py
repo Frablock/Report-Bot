e347aa29b548a6b63b9355c3f8c1056c274f530f
Frablock <53878484+Frablock@users.noreply.github.com>
2024-11-21 13:57:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ async def getprofileinfo(ctx: SlashContext, pseudo):
 
     await ctx.send("Here is all the report that concern the provided user")
     for d in data:
-        await ctx.send("## "+str(5)+" sur "+str(6)+"\n> Raison : "+str(d[4])+"\n> Post problématique : [Post original](<"+str(d[2])+">)  [Lien archive.org](<"+str(d[3])+">)\n> Statut : à implémenter")
+        await ctx.send("## "+str(d[5])+" sur "+str(d[6])+"\n> Raison : "+str(d[4])+"\n> Post problématique : [Post original](<"+str(d[2])+">)  [Lien archive.org](<"+str(d[3])+">)\n> Statut : à implémenter")
 
 
 
